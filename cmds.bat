0d8b2fcf2f017e297f8c390ef8e55998c95d8a72
Mathis WAUQUIEZ <mathis.wauquiez@gmail.com>
2023-09-06 14:01:24 +0200

    V1

---
+++ cmds.bat
@@ -0,0 +1,8 @@
+conda activate HackatonENPC
+set FLASK_APP=main.py
+set FLASK_ENV=development
+git clone https://github.com/CelestinHans/Hackaton_team.git
+git remote add upstream https://github.com/CelestinHans/Hackaton_team.git
+git checkout
+git checkout Mathis
+git add --all
